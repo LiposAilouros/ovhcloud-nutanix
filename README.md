87af0123231394dddc4c53eac2471b870867df2b
LiposAilouros <maxime.lacan@free.fr>
2024-03-30 09:35:01 +0100

    Check API script

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # ovhcloud-nutanix
-Some tools in order to manage nutanix cluster on OVHcloud
+Some tools in order to manage nutanix cluster on OVHcloud.
+
+# Disclaimer
+This bash tools are personal.
+Use it in lab, fork, customize and enjoy.
+
+# Used configuration 
+Script are used on MacOS and Ubuntu.
+IMPORTANT : you need to install JQ https://jqlang.github.io/jq/ (1.6 on my Ubuntu laptop as I'm writing this file)
+
+# Generate Token for OVHcloud API
+You need to generate a token in order to be able to use ovhcloud API for script who uses this API.
+
+https://eu.api.ovh.com/createToken/ for EU
+Documentation avalaible here : https://help.ovhcloud.com/csm/fr-api-getting-started-ovhcloud-api?id=kb_article_view&sysparm_article=KB0042789#utilisation-avancee-coupler-les-api-ovhcloud-avec-une-application
+
+https://ca.api.ovh.com/createToken/ for CA
+Documentation avalaible here : https://help.ovhcloud.com/csm/en-ca-api-getting-started-ovhcloud-api?id=kb_article_view&sysparm_article=KB0029722#advanced-usage-pair-ovhcloud-apis-with-an-application
+
+https://api.us.ovhcloud.com/createToken/ for US
+
+Once generated use secret.cfg.template[REGION] to save your acces token and save to secret.cfg.
+
+# Test your Token
---
+++ checkapi.sh
@@ -0,0 +1,53 @@
+#!/bin/bash                                                                                                                                                                                                                                                           
+#set -eux
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+GREEN='\033[0;32m'        # Green
+BYellow='\033[1;33m'      # Bold Yellow
+set -o pipefail
+
+CURL() {
+    METHOD=$1
+    QUERY=${ENDPOINT}$2
+    TSTAMP=$(date +%s)
+    if [ $METHOD == "GET" ] || [ $METHOD == "DELETE" ]
+        then
+            BODY=""
+            SHA=$(echo -n $AS+$CK+$METHOD+$QUERY+$BODY+$TSTAMP | shasum | cut -d ' ' -f 1)
+            SIGNATURE="\$1\$$SHA"
+            fnret=$(curl -s -X $METHOD -H "Content-type: application/json" -H "X-Ovh-Application: $AK" -H "X-Ovh-Consumer: $CK" -H "X-Ovh-Signature: $SIGNATURE" -H "X-Ovh-Timestamp: $TSTAMP" $QUERY)
+            echo ${fnret} | jq .
+        else
+            BODY=$3
+            SHA=$(echo -n $AS+$CK+$METHOD+$QUERY+$BODY+$TSTAMP | shasum | cut -d ' ' -f 1)
+            SIGNATURE="\$1\$$SHA"
+            fnret=$(curl -s -X $METHOD -H "Content-type: application/json" -H "X-Ovh-Application: $AK" -H "X-Ovh-Consumer: $CK" -H "X-Ovh-Signature: $SIGNATURE" -H "X-Ovh-Timestamp: $TSTAMP" "${QUERY}" --data "${BODY}")
+            echo ${fnret} | jq .
+    fi
+}
+
+# Checking token file
+if [ ! -f "$(pwd)/secret.cfg" ]; then
+    echo -e "${RED}Can't find $(pwd)/secret.cfg file${NC}"
+    exit 1
+fi
+
+# Source token and endpoint
+source $(pwd)/secret.cfg
+
+# Checking Token
+nic=$(CURL GET "/1.0/me" | jq -r .nichandle)
+if [ ! ${nic} ] || [ ${nic} == null ]; then
+    echo -e "${RED}Unable to fetch your nichandle${NC}"
+    if [ ! ${AK} ] || [ ! ${CK} ] || [ ! ${ENDPOINT} ] || [ ! ${AS} ]; then
+        echo -e "${RED}Missing entry in secret.cfg${NC}"
+        echo -e "${BYellow}Example bellow${NC}"
+        echo -e "AK='*********************'\nAS='*********************'\nCK='*********************'\nENDPOINT='https://eu.api.ovh.com'"
+    else
+        echo -e "${RED}Check your API token and connectivity to ${ENDPOINT}${NC}"
+    fi
+    exit 1
+else
+    echo -e "${GREEN}Api communication is OK \nYour nic is : ${nic}${NC}"
+fi
+
